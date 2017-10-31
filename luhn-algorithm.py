533faf4ac6905b899f47236023b6b36d4f408257
colyk <colyk@yandex.ru>
2017-10-31 13:27:38 +0100

    clean up

--- luhn-algorithm.py
+++ luhn-algorithm.py
@@ -1,45 +1,60 @@
 import random
-#def sumNumb(str):return sum(map(lambda x: int(x),str)) print(sumNumb("12"))
-    
-def sumNum(number):
+
+def get_sum(number):
     return number//10+number%10
 
-def luna(cardNumber):
-    evenNumbers = cardNumber[::2]
-    oddNumbers = list(cardNumber[1::2])
-    sumOddNumbers = 0
-    for i in oddNumbers:
-        sumOddNumbers+=int(i)
+
+def luhn(card_number):                # return 0 if card number is valide
+    even_numbers = card_number[::2]
+    odd_numbers = list(card_number[1::2])
+    sum_odd_numbers = 0
     k = 0
-    summ = 0
-    for i in evenNumbers:
+    digits_sum = 0
+
+    for i in odd_numbers:
+        sum_odd_numbers += int(i)
+    for i in even_numbers:
         k = int(i)*2
-        summ+=sumNum(k)
-    return(summ+sumOddNumbers)%10 
+        digits_sum += get_sum(k)
+    return (digits_sum+sum_odd_numbers)%10 
 
-def generateCardNumber():
+
+def get_new_number():
     result = '4'
-    for i in range(2,16):
+    for i in range(2, 16):
         random.seed()
-        result+=str(random.randint(1,9))
-        if(i%4 == 0):
-            result+=' '
-    #print(result)
-    for i in range(0,9):
-        if(luna((result+str(i)).replace(" ",'')) == 0):
-            result+=str(i)
+        result += str(random.randint(0, 9))
+        if i%4 == 0:
+            result += ' '
+            
+    for i in range(0, 9):
+        if luhn((result+str(i)).replace(' ', '')) == 0:
+            result += str(i)
             return result
+    return get_new_number()
+
         
-
-cardNumber = input("Put you card number to check: ").replace(" ",'')
-if(len(cardNumber)!=16 or not cardNumber.isdigit()):
-    print("Incorrect card number!")
-    exit()
-else:
-    if(luna(cardNumber) == 0):
+def check_card_number(number):
+    if len(number) == 0:
         print("Incorrect card number!")
-    else:
+        return
+    card_number = number.replace(' ', '')
+    if len(card_number) != 16 or not card_number.isdigit():
+        print("Incorrect card number!")
+        return
+    if luhn(card_number) == 0:
         print("Correct card number!")
-print(generateCardNumber())
+    else:
+        print("Incorrect card number!")
 
-input()
+
+def main():
+    card = input("Put you card number to check: ")
+    check_card_number(card)
+    valide_card = get_new_number()
+    print(valide_card)
+
+
+if __name__ == '__main__':
+    main()
+    input()
