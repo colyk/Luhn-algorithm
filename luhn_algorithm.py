ddbc514d52f4e3519d9959ed0c71cb82f5f8575e
colyk <colyk@yandex.ru>
2018-04-18 13:22:37 +0200

    review

--- luhn_algorithm.py
+++ luhn_algorithm.py
@@ -1,10 +1,11 @@
 import random
 
+
 def get_sum(number):
-    return number//10+number%10
+    return number // 10 + number % 10
 
 
-def luhn(card_number):                # return 0 if card number is valide
+def is_correct(card_number):
     even_numbers = card_number[::2]
     odd_numbers = list(card_number[1::2])
     sum_odd_numbers = 0
@@ -14,35 +15,35 @@ def luhn(card_number):                # return 0 if card number is valide
     for i in odd_numbers:
         sum_odd_numbers += int(i)
     for i in even_numbers:
-        k = int(i)*2
+        k = int(i) * 2
         digits_sum += get_sum(k)
-    return (digits_sum+sum_odd_numbers)%10 
+    return not ((digits_sum + sum_odd_numbers) % 10)
 
 
-def get_new_number():
+def generate_card_number():
     result = '4'
     for i in range(2, 16):
         random.seed()
         result += str(random.randint(0, 9))
-        if i%4 == 0:
+        if i % 4 == 0:
             result += ' '
-            
+
     for i in range(0, 9):
-        if luhn((result+str(i)).replace(' ', '')) == 0:
+        if is_correct((result + str(i)).replace(' ', '')):
             result += str(i)
             return result
-    return get_new_number()
+    return generate_card_number()
 
-        
+
 def check_card_number(number):
-    if len(number) == 0:
+    if not number:
         print("Incorrect card number!")
         return
     card_number = number.replace(' ', '')
     if len(card_number) != 16 or not card_number.isdigit():
         print("Incorrect card number!")
         return
-    if luhn(card_number) == 0:
+    if is_correct(card_number):
         print("Correct card number!")
     else:
         print("Incorrect card number!")
@@ -51,10 +52,8 @@ def check_card_number(number):
 def main():
     card = input("Put you card number to check: ")
     check_card_number(card)
-    valide_card = get_new_number()
-    print(valide_card)
+    print('Generated card number: %s' % generate_card_number())
 
 
 if __name__ == '__main__':
     main()
-    input()
